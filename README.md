78233de94a22f2c6ad46fbc65dbe438882edf6fd
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-09-06 05:37:08 +0530

    create action

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
  2 = create migration (rails g migration create_articles)
 
  3 = welcome controller (rails g controller home index)
+
+ 4 = article controller,model,index view
+     articles_controller.rb
+     article.rb
+     articles articles.html.erb
+
+5 = article create and new action
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,24 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end 
